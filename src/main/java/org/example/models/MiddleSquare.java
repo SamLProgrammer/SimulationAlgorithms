b4c755a4066bacad1d88b3eff6f9818d3897ee12
SamLProgrammer <samuel.lopez@uptc.edu.co>
2023-08-12 13:22:46 -0500

    middle-squares and congruent methods merged to Sheets Integration core code

---
+++ src/main/java/org/example/models/MiddleSquare.java
@@ -0,0 +1,55 @@
+package org.example.models;
+
+import java.util.ArrayList;
+
+public class MiddleSquare {
+
+    public int getSeed(int length) {
+        int random;
+        do {
+            random = (int)(Math.random()*Math.pow(10,length));
+        }while(String.valueOf(random).length() < 4);
+        return random;
+    }
+
+    public int intToSquare(int number) {
+        return number*number;
+    }
+
+    public ArrayList<Double> fecadeStartSquaresGeneration(int iterations) {
+        int length = 4;
+        int seed = getSeed(length);
+        ArrayList<Double> risList = new ArrayList<>();
+        return startSquaresGeneration("3456", iterations, risList);
+    }
+    public ArrayList<Double> startSquaresGeneration(String seedString, int iterations, ArrayList<Double> risList) {
+        if(iterations > 0) {
+            System.out.println("=========== Iteration " + iterations + " ===========");
+            int seed = Integer.valueOf(seedString);
+            int sqrdSeed = intToSquare(seed);
+            String stringSqrdSeed = String.valueOf(sqrdSeed);
+
+            while(stringSqrdSeed.length() < (seedString.length() * 2)) {
+                stringSqrdSeed = '0' + stringSqrdSeed;
+            }
+            System.out.println("stringSqrdSeed");
+            System.out.println(stringSqrdSeed);
+
+            int firstPointer = stringSqrdSeed.length() / 2 - seedString.length() / 2;
+            int secondPointer = firstPointer + seedString.length();
+
+            String nextSeed = "";
+            for (int i = firstPointer; i < secondPointer; i++) {
+                nextSeed += stringSqrdSeed.charAt(i);
+            }
+            System.out.println("======================================");
+            System.out.println("Next Seed: ");
+            System.out.println(nextSeed);
+            risList.add(Double.valueOf(nextSeed)/Math.pow(10,nextSeed.length()));
+
+            return startSquaresGeneration(nextSeed, iterations-1, risList);
+        }
+        return risList;
+    }
+}
+
